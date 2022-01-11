e5e59dd5268bc90fb70553b423a32929a3bc0baa
eloy <97523367+eloy9111@users.noreply.github.com>
2022-01-11 20:19:04 +0800

    Add files via upload

---
+++ TDG8t9RZ.js
@@ -0,0 +1,30 @@
+1: SendLargeMessages (Split Large Messages)
+- https://bit.ly/3l58vLe
+
+2: AccountSwitcher
+- https://bit.ly/3sXjqHX
+
+3: DirectDownload (Download with Ctrl + S)
+- https://bit.ly/3f68we9
+
+4: GoogleTranslateOption (Translator)
+- https://bit.ly/377m6d4
+
+5: WriteUpperCase
+- https://bit.ly/39NiRcc
+
+6: ServerFolders
+- https://bit.ly/3dFWabg
+
+7: Game Activity Toggle
+- https://bit.ly/3f3m7mH
+
+8: TypingIndicator
+- https://bit.ly/3mpbxsB
+(if the first one doesnt work): https://bit.ly/32bAn5X
+
+9: ImageUtilities
+- https://bit.ly/3rZN4uW
+
+10: PermissionsViewer
+- https://bit.ly/31UMDrs
